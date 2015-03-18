5bc6fe6a5543592f36acdb8e89f51bc42ecc8454
thedark <thedark@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-18 19:28:00 +0000

    adapted for Wordpress 4.1.1
    git-svn-id: https://plugins.svn.wordpress.org/tampile-temperature-conversion-widget/tags/1.2@1115487 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tampile.php
+++ tampile.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: Tampile temperature converter
-Plugin URI: http://www.tampile.com
+Plugin URI: http://www.flamescorpion.com
 Description: Convert any temperature: fahrenheit, celsius, kelvin and rankin.
 Author: Lucian Apostol
-Version: 1
-Author URI: http://www.tampile.com
+Version: 1.2
+Author URI: http://www.flamescorpion.com
 */
 
 
@@ -98,7 +98,7 @@ function widgetTampile($args) {
 
 function tampileInit()
 {
-  register_sidebar_widget(__('Tampile'), 'widgetTampile');     
+  register_sidebar_widget(__('Tampile Temperature Converter'), 'widgetTampile');     
 }
 add_action("plugins_loaded", "tampileInit");
 ?>
